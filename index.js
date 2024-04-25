ca0494c3fe775ce63174d2cf799a4d710345b7ea
Paulo Henrique Santos <phqnrz@gmail.com>
2024-04-24 22:10:06 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// funcionalidade de revelar os atributos da página
+window.revelar = ScrollReveal({ reset: true });
+
+// topo do site (first section)
+revelar.reveal(".efeito-txt-topo", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '8rem'
+});
