72979edeeec30307ea57104e7d4481906f4a9577
Paulo Henrique <phqnrz@gmail.com>
2024-05-10 11:52:45 -0300

    ajustes gerais

--- scripts/index.js
+++ scripts/index.js
@@ -1,159 +1,9 @@
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
-
-// funcionalidade de revelar os atributos da página
-window.revelar = ScrollReveal({ reset: true });
-
-// topo do site (first section)
-revelar.reveal(".efeito-txt-topo", {
-  duration: 1200, // sempre colocar segundos * 1000
-  distance: '8rem'
+// alterar tema (escuro / claro)
+const theme = document.getElementById('theme');
+theme.addEventListener('change', () => {
+  document.body.classList.toggle('dark-theme')
 });
-
-//  reveal sobre
-revelar.reveal(".efeito-sobre", {
-  duration: 2200, // sempre colocar segundos * 1000
-  distance: '8rem',
-  delay: 500
-});
-
-revelar.reveal(".github", {
-  duration: 2200, // sempre colocar segundos * 1000
-  distance: '8rem',
-  delay: 500,
-  origin: 'right'
-});
-
-revelar.reveal(".linkedin", {
-  duration: 2200, // sempre colocar segundos * 1000
-  distance: '8rem',
-  delay: 1000,
-  origin: 'right'
-});
-
-
-
-
-
-// reveal habilidades
-revelar.reveal(".efeito-habilidades", {
-  duration: 2500, // sempre colocar segundos * 1000
-  distance: '8rem'
-});
-
-revelar.reveal(".html", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 500,
-  origin: 'right'
-});
-
-revelar.reveal(".css", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1000,
-  origin: 'right'
-});
-
-revelar.reveal(".bulma", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1500,
-  origin: 'right'
-});
-
-revelar.reveal(".bootstrap", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 2000,
-  origin: 'right'
-});
-
-revelar.reveal(".javascript", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 2500,
-  origin: 'right'
-});
-
-// em desenvolvimento
-revelar.reveal(".efeito-habilidades", {
-  duration: 2200, // sempre colocar segundos * 1000
-  distance: '8rem',
-  delay: 500
-});
-
-
-revelar.reveal(".typescript", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 500,
-  origin: 'left'
-});
-
-revelar.reveal(".react", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1000,
-  origin: 'left'
-});
-
-revelar.reveal(".node", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1500,
-  origin: 'left'
-});
-
-revelar.reveal(".java", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 2000,
-  origin: 'left'
-});
-
-revelar.reveal(".mongodb", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 2500,
-  origin: 'left'
-});
-
-
-
-// efeito revelar projetos
-revelar.reveal(".efeito-projetos", {
-  duration: 2200, // sempre colocar segundos * 1000
-  distance: '4rem'
-});
-
-revelar.reveal(".port1", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 500,
-  origin: 'right'
-});
-
-revelar.reveal(".port2", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1000,
-  origin: 'right'
-});
-
-revelar.reveal(".port3", {
-  duration: 2000, // sempre colocar segundos * 1000
-  distance: '6rem',
-  delay: 1500,
-  origin: 'right'
-});
-
-
-
+//---------------------------------------------------------
 
 
 // efeito máquina de escrever
@@ -166,9 +16,9 @@ function typeWriter(elemento) {
   });
 }
 
-const nome = document.querySelector(".nome");
+const nome = document.querySelector(".meuNome");
 typeWriter(nome);
-
+//-----------------------------------------------------------
 
 
 //barra de rolagem 
@@ -176,3 +26,192 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector('#header');
   header.classList.toggle('scrolling', window.scrollY > 0);
 });
+
+//------------------------------------------------------------
+
+
+
+// menu mobile
+var btnMenu = document.getElementById("btn-menu");
+var menuMobile = document.getElementById("menu-mobile");
+var overlay = document.getElementById("overlay-menu");
+var btnFechar = document.getElementById("btn-fechar");
+
+
+btnMenu.addEventListener("click", () => {
+  menuMobile.classList.add("abrir-menu");
+});
+
+btnFechar.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
+
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
+
+
+
+
+
+
+
+
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+//   });
+// }
+
+// // funcionalidade de revelar os atributos da página
+// window.revelar = ScrollReveal({ reset: true });
+
+// // topo do site (first section)
+// revelar.reveal(".efeito-txt-topo", {
+//   duration: 1200, // sempre colocar segundos * 1000
+//   distance: '8rem'
+// });
+
+// //  reveal sobre
+// revelar.reveal(".efeito-sobre", {
+//   duration: 2200, // sempre colocar segundos * 1000
+//   distance: '8rem',
+//   delay: 500
+// });
+
+// revelar.reveal(".github", {
+//   duration: 2200, // sempre colocar segundos * 1000
+//   distance: '8rem',
+//   delay: 500,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".linkedin", {
+//   duration: 2200, // sempre colocar segundos * 1000
+//   distance: '8rem',
+//   delay: 1000,
+//   origin: 'right'
+// });
+
+
+
+
+
+// // reveal habilidades
+// revelar.reveal(".efeito-habilidades", {
+//   duration: 2500, // sempre colocar segundos * 1000
+//   distance: '8rem'
+// });
+
+// revelar.reveal(".html", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 500,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".css", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1000,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".bulma", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1500,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".bootstrap", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 2000,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".javascript", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 2500,
+//   origin: 'right'
+// });
+
+// // em desenvolvimento
+// revelar.reveal(".efeito-habilidades", {
+//   duration: 2200, // sempre colocar segundos * 1000
+//   distance: '8rem',
+//   delay: 500
+// });
+
+
+// revelar.reveal(".typescript", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 500,
+//   origin: 'left'
+// });
+
+// revelar.reveal(".react", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1000,
+//   origin: 'left'
+// });
+
+// revelar.reveal(".node", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1500,
+//   origin: 'left'
+// });
+
+// revelar.reveal(".java", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 2000,
+//   origin: 'left'
+// });
+
+// revelar.reveal(".mongodb", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 2500,
+//   origin: 'left'
+// });
+
+
+
+// // efeito revelar projetos
+// revelar.reveal(".efeito-projetos", {
+//   duration: 2200, // sempre colocar segundos * 1000
+//   distance: '4rem'
+// });
+
+// revelar.reveal(".port1", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 500,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".port2", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1000,
+//   origin: 'right'
+// });
+
+// revelar.reveal(".port3", {
+//   duration: 2000, // sempre colocar segundos * 1000
+//   distance: '6rem',
+//   delay: 1500,
+//   origin: 'right'
+// });
+
